7e2bd0266bdc3c5b8911ba85edd3f09ac640e71b
이욱재 <ukjae.lee@mirero.co.kr>
2023-04-10 10:27:16 +0900

    Update Program.cs

--- TempConsolePractice/Program.cs
+++ TempConsolePractice/Program.cs
@@ -5,7 +5,7 @@ public class Program
     public static void Main(string[] args)
     {
         var obj = new Program();
-        var pointContainer = obj.CreateRegularPolygonPointContainer(new PointDouble(1, 1), 4, 5);
+        var pointContainer = obj.CreateRegularPolygon(new PointDouble(1, 1), 4, 5);
 
         foreach (var point in pointContainer)
         {
@@ -37,10 +37,10 @@ public class Program
         }
     }
 
-    public PointDouble[] CreateRegularPolygonPointContainer(PointDouble center, double circumCircleRadius, int totalSides)
+    public PointDouble[] CreateRegularPolygon(PointDouble center, double circumRadius, int totalSides)
     {
         // CircumCircle, InCircle, CircumCenter, InCenter, CircumRadius, InRadius.
-        var pointContainer = new PointDouble[totalSides];
+        var regularPolygon = new PointDouble[totalSides];
         var centralAngleRadian = 2 * Math.PI / totalSides;
 
         for (var i = 0; i < totalSides; ++i)
@@ -49,11 +49,51 @@ public class Program
             // Reflecteing the coordinate over y = x will cuase problem with WriteableBitmap because the order of points is changed.
             // In shorts. don't use (sinθ, cosθ) instead of (cosθ, sinθ).
             // Translate by center(x,y).
-            var x = circumCircleRadius * Math.Sin(i * centralAngleRadian) + center.X;
-            var y = circumCircleRadius * Math.Cos(i * centralAngleRadian) + center.Y;
-            pointContainer[i] = new PointDouble(x, y);
+            var x = circumRadius * Math.Sin(i * centralAngleRadian) + center.X;
+            var y = circumRadius * Math.Cos(i * centralAngleRadian) + center.Y;
+            regularPolygon[i] = new PointDouble(x, y);
         }
 
-        return pointContainer;
+        return regularPolygon;
+    }
+
+    public PointDouble[] RotatePointContainer(PointDouble[] pointContainer, PointDouble center, double angleRadian)
+    {
+        var rotatedPointContainer = new PointDouble[pointContainer.Length];
+
+        for (var i = 0; i < pointContainer.Length; ++i)
+        {
+            var rotatedPoint = RotatePoint(pointContainer[i], center, angleRadian);
+            rotatedPointContainer[i] = rotatedPoint;
+        }
+
+        return rotatedPointContainer;
+    }
+
+    public PointDouble RotatePoint(PointDouble point, PointDouble center, double angleRadian)
+    {
+        var cos = Math.Cos(angleRadian);
+        var sin = Math.Sin(angleRadian);
+
+        var rotatedX = (point.X - center.X) * cos - (point.Y - center.Y) * sin + center.X;
+        var rotatedY = (point.X - center.X) * sin + (point.Y - center.Y) * cos + center.Y;
+
+        return new PointDouble(rotatedX, rotatedY);
+    }
+
+    public int[] ConvertToRegularPolygonInt(PointDouble[] regularPolygon)
+    {
+        var regularPolygonInt = new int[2 * regularPolygon.Length];
+
+        // [WIP]
+        for (var i = 0; i < regularPolygon.Length; ++i)
+        {
+            var x = (int)Math.Round(regularPolygon[i].X);
+            var y = (int)Math.Round(regularPolygon[i].Y);
+            regularPolygonInt[2 * i] = x;
+            regularPolygonInt[2 * i + 1] = y;
+        }
+
+        return regularPolygonInt;
     }
 }
